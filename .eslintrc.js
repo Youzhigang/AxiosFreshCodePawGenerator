d6bc4fe0ff5eafe96e1ea28ab739d95d695263f2
youzhigang <youzhigang@suiyi.com.cn>
2020-02-06 21:32:55 +0800

    chore: update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,12 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['eslint:recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 2018
   },
   plugins: [],
